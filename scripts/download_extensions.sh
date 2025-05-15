b9010bf363960a8407f957cc80fc061647d4cb32
tobilg <tobilg@gmail.com>
2025-05-15 13:04:52 +0200

    Also include Iceberg extension

--- scripts/download_extensions.sh
+++ scripts/download_extensions.sh
@@ -6,8 +6,10 @@ mkdir -p $PWD/extensions
 curl http://extensions.duckdb.org/v1.2.1/linux_amd64/httpfs.duckdb_extension.gz --output $PWD/extensions/httpfs.duckdb_extension.gz
 curl http://extensions.duckdb.org/v1.2.1/linux_amd64/json.duckdb_extension.gz --output $PWD/extensions/json.duckdb_extension.gz
 curl http://extensions.duckdb.org/v1.2.1/linux_amd64/arrow.duckdb_extension.gz --output $PWD/extensions/arrow.duckdb_extension.gz
+curl http://nightly-extensions.duckdb.org/v1.2.1/linux_amd64/iceberg.duckdb_extension.gz --output $PWD/extensions/iceberg.duckdb_extension.gz
 
 # Unzip
 gunzip $PWD/extensions/httpfs.duckdb_extension.gz
 gunzip $PWD/extensions/json.duckdb_extension.gz
 gunzip $PWD/extensions/arrow.duckdb_extension.gz
+gunzip $PWD/extensions/iceberg.duckdb_extension.gz
